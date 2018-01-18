21ac287c0cd5d489333ffba4e4e02fe1299495b5
Dustin Pierson <dustinapierson@gmail.com>
2018-01-18 10:26:12 -0600

    created getElement

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ let iContainer = document.getElementById("inputContainer");
 
 let sContainer = document.getElementById("shapeContainer");
 
+let cSquare = document.getElementById("square");
+
+let cRectangle = document.getElementById("rectangle");
+
+let cCircle = document.getElementById("circle");
+
+let cTriangle = document.getElementById("triangle");
+
+let sLength = document.getElementById("squareLength");
+
+let hRectangle = document.getElementById("rectangleHeight");
+
+let wRectangle = document.getElementById("rectangleWidth");
+
+let dCircle = document.getElementById("circleDiameter");
+
+let hTriangle = document.getElementById("triangleHeight");
+
